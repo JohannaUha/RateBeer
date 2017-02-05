f47c4c086957cc4304ae0be39b96341c72b54894
JohannaUha <Uha@Johannas-MacBook-Pro.local>
2017-02-05 14:41:41 +0200

    vko3teht6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,7 +2,9 @@ class Brewery < ActiveRecord::Base
   include RatingAverage
 
   validates :name, presence: true
-  validates :year, inclusion: { in: 1042..2017 }
+  validates :year, numericality: { greater_than_or_equal_to: 1042,
+                                   less_than_or_equal_to: Proc.new { Time.now.year },
+                                   only_integer: true }
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
@@ -17,7 +19,6 @@ class Brewery < ActiveRecord::Base
     self.year = 2017
     puts "changed year to #{year}"
   end
-
 
 
 end
