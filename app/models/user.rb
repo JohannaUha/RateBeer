561ef9eb6848b61b15433b43760307d9fb0fc0c6
JohannaUha <Uha@Johannas-MacBook-Pro.local>
2017-02-07 11:13:49 +0200

    testejaMateriaalista

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   has_many :beer_clubs, through: :memberships
 
   def favorite_beer
+    return nil if ratings.empty?
+    ratings.order(score: :desc).limit(1).first.beer
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+def create_beer_with_rating(user, score)
+  beer = FactoryGirl.create(:beer)
+  FactoryGirl.create(:rating, score:score, beer:beer, user:user)
+  beer
+end
+
+def create_beers_with_ratings(user, *scores)
+  scores.each do |score|
+    create_beer_with_rating(user, score)
+  end
+end
+
 RSpec.describe User, type: :model do
   it "has the username set correctly" do
     user = User.new username:"Pekka"
@@ -26,6 +38,7 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq(0)
   end
 
+
   describe "favorite beer" do
     let(:user){FactoryGirl.create(:user) }
 
@@ -35,6 +48,19 @@ RSpec.describe User, type: :model do
 
     it "without ratings does not have one" do
       expect(user.favorite_beer).to eq(nil)
+    end
+
+    it "is the only rated if only one rating" do
+      beer = create_beer_with_rating(user, 10)
+
+      expect(user.favorite_beer).to eq(beer)
+    end
+
+    it "is the one with highest rating if several rated" do
+      create_beers_with_ratings(user, 10, 20, 15, 7, 9)
+      best = create_beer_with_rating(user, 25)
+
+      expect(user.favorite_beer).to eq(best)
     end
   end
 
@@ -55,4 +81,6 @@ RSpec.describe User, type: :model do
       expect(user.average_rating).to eq(15.0)
     end
   end
+
+
 end
