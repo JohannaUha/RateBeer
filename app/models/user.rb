dc9aa8e3b72187cbd4c4e6bd461350e2770dbdb0
JohannaUha <Uha@Johannas-MacBook-Pro.local>
2017-02-12 22:27:45 +0200

    4.3-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,14 @@ class User < ActiveRecord::Base
     ratings.order(score: :desc).limit(1).first.beer
   end
 
+  def favorite_style
+    return nil if ratings.empty?
+    return Beer.find_by_sql("SELECT beers.*, SUM(ratings.score) as sum FROM beers JOIN ratings ON beers.id = ratings.beer_id JOIN users ON ratings.user_id = users.id WHERE ratings.user_id = " + id.to_s + " GROUP BY beers.style ORDER BY sum DESC LIMIT 1").first.style
+  end
+
+  def favourite_brewery
+    return nil if ratings.empty?
+    return Beer.find_by_sql("SELECT beers.*, SUM(ratings.score) as sum FROM beers JOIN ratings ON beers.id = ratings.beer_id JOIN users ON ratings.user_id = users.id JOIN breweries ON breweries.id = beers.brewery_id WHERE ratings.user_id = " + id.to_s + " GROUP BY breweries.id ORDER BY sum DESC LIMIT 1").first.brewery.name
+  end
+
 end
